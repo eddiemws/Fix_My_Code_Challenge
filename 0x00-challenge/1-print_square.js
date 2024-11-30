4f9ba65e2750f52376e3df9e7835da79f0994137
eddy <106661971+eddiemws@users.noreply.github.com>
2024-11-30 23:38:22 +0300

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,11 +14,11 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10)
+
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
-    
+    }
     process.stdout.write("\n");
 }
