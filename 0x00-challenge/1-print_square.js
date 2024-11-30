6f8f1d17fc6f13e23eebedeed5e830a84130e01a
eddy <106661971+eddiemws@users.noreply.github.com>
2024-11-30 23:39:06 +0300

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -7,7 +7,7 @@
 */
 
 
- {
+ if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
